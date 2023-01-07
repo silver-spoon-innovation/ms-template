ca8eb7c1115c590dc58ec91ec08cb86413a165d3
pamornpolh <pamornpol.hanmatheekuna@gmail.com>
2023-01-08 01:22:55 +0700

    debugging

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/lib/"],
   moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',
-    '^@mock/(.*)$': '<rootDir>/mock/$1'
-  }
+    "^@src/(.*)$": "<rootDir>/src/$1",
+    "^@mock/(.*)$": "<rootDir>/mock/$1",
+  },
 };
